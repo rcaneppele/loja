b766acbcc7ceb43f9e4059ee6a37f7856982d6a0
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-01-14 14:31:47 -0200

    Annotation para definir qual Controller nao precisa de autenticacao

---
+++ src/main/java/br/com/rcaneppele/loja/login/seguranca/AcessoPublico.java
@@ -0,0 +1,12 @@
+package br.com.rcaneppele.loja.login.seguranca;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AcessoPublico {
+
+}
