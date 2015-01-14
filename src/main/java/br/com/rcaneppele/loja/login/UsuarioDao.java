2c06d6f450b8710e8743e098f6e5c37d0f6e6c6b
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-01-14 14:28:33 -0200

    classe UsuarioDao

---
+++ src/main/java/br/com/rcaneppele/loja/login/UsuarioDao.java
@@ -0,0 +1,26 @@
+package br.com.rcaneppele.loja.login;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class UsuarioDao {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public Usuario buscaPorLoginESenha(String login, String senha) {
+		String jpql = "FROM Usuario WHERE login = :login AND senha = :senha";
+		try {
+			return manager.createQuery(jpql, Usuario.class)
+					.setParameter("login", login)
+					.setParameter("senha", senha)
+					.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+	
+}
