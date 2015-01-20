b407ef220d063a436d6bc22fadf8c1eeb90b6207
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-01-20 10:30:59 -0200

    cadastro/alteracao de usuarios

--- src/main/java/br/com/rcaneppele/loja/usuario/UsuariosController.java
+++ src/main/java/br/com/rcaneppele/loja/usuario/UsuariosController.java
@@ -1,13 +1,18 @@
 package br.com.rcaneppele.loja.usuario;
 
+import java.util.ResourceBundle;
+
 import javax.inject.Inject;
+import javax.validation.Valid;
 
 import org.hibernate.validator.constraints.NotBlank;
 
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Put;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.validator.I18nMessage;
 import br.com.caelum.vraptor.validator.Validator;
 
 @Controller
@@ -18,6 +23,9 @@ public class UsuariosController {
 	
 	@Inject
 	private Validator validator;
+	
+	@Inject
+	private ResourceBundle bundle;
 	
 	@Inject
 	private UsuarioDao usuarioDao;
@@ -39,6 +47,52 @@ public class UsuariosController {
 		
 		result.include("usuarios", usuarioDao.buscaPorLoginLike(login));
 		result.of(this).usuarios();
+	}
+	
+	@Get("/usuarios/novo")
+	public void novoUsuario() {
+	}
+
+	@Post("/usuarios")
+	public void salva(@Valid Usuario usuario) {
+		redirecionaParaPaginaDeCadastroCasoOcorraErroDeValidacao(usuario);
+		
+		validaUsuarioJaCadastrado(usuario);
+
+		usuarioDao.salva(usuario);
+		result.include("info", bundle.getString("info.cadastro.sucesso"));
+		result.redirectTo(this).usuarios();
+	}
+	
+	@Get("/usuarios/editar/{id}")
+	public void edita(Long id) {
+		result.include("usuario", usuarioDao.buscaPorId(id));
+		result.forwardTo(this).novoUsuario();
+	}
+	
+	@Put("/usuario")
+	public void altera(@Valid Usuario usuario) {
+		redirecionaParaPaginaDeCadastroCasoOcorraErroDeValidacao(usuario);
+		
+		validaUsuarioJaCadastrado(usuario);
+
+		usuarioDao.altera(usuario);
+		result.include("info", bundle.getString("info.alteracao.sucesso"));
+		result.redirectTo(this).usuarios();
+	}
+	
+	private void validaUsuarioJaCadastrado(Usuario usuario) {
+		Usuario existente = usuarioDao.buscaPorLogin(usuario.getLogin());
+		validator.addIf(existente != null && !existente.equals(usuario), new I18nMessage("error", "usuario.erro.cadastro.duplicado"));
+		redirecionaParaPaginaDeCadastroCasoOcorraErroDeValidacao(usuario);
+	}
+
+	private void redirecionaParaPaginaDeCadastroCasoOcorraErroDeValidacao(Usuario usuario) {
+		if (usuario.getId() == null) {
+			validator.onErrorRedirectTo(this).novoUsuario();
+		} else {
+			validator.onErrorRedirectTo(this).edita(usuario.getId());
+		}
 	}
 	
 }
