68331949b7a3a870cf9eb64c4c92d03035afc07c
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-01-20 09:55:40 -0200

    busca paginada e count no UsuarioDao

--- src/main/java/br/com/rcaneppele/loja/usuario/UsuarioDao.java
+++ src/main/java/br/com/rcaneppele/loja/usuario/UsuarioDao.java
@@ -1,15 +1,23 @@
 package br.com.rcaneppele.loja.usuario;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+
+import br.com.rcaneppele.loja.util.Paginacao;
 
 @Stateless
 public class UsuarioDao {
 
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Inject
+	private Paginacao paginacao;
 	
 	public Usuario buscaPorLoginESenha(String login, String senha) {
 		String jpql = "FROM Usuario WHERE login = :login AND senha = :senha";
@@ -18,6 +26,23 @@ public class UsuarioDao {
 					.setParameter("login", login)
 					.setParameter("senha", senha)
 					.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+
+	public List<Usuario> buscaPaginada(int pagina) {
+		String query = "FROM Usuario ORDER BY login";
+		return manager.createQuery(query, Usuario.class)
+				.setFirstResult((pagina - 1) * paginacao.getPageSize())
+				.setMaxResults(paginacao.getPageSize())
+				.getResultList();
+	}
+	
+	public Long count() {
+		String query = "SELECT COUNT(*) FROM Usuario";
+		try {
+			return manager.createQuery(query, Long.class).getSingleResult();
 		} catch (NoResultException e) {
 			return null;
 		}
