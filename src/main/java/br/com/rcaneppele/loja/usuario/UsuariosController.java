741547d7a1195b163a523cb48b2014abe4a0bf93
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-01-20 10:07:34 -0200

    metodo para filtrar listagem de usuarios

--- src/main/java/br/com/rcaneppele/loja/usuario/UsuariosController.java
+++ src/main/java/br/com/rcaneppele/loja/usuario/UsuariosController.java
@@ -2,15 +2,22 @@ package br.com.rcaneppele.loja.usuario;
 
 import javax.inject.Inject;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.validator.Validator;
 
 @Controller
 public class UsuariosController {
 
 	@Inject
 	private Result result;
+	
+	@Inject
+	private Validator validator;
 	
 	@Inject
 	private UsuarioDao usuarioDao;
@@ -24,6 +31,14 @@ public class UsuariosController {
 	public void usuarios(int pagina) {
 		result.include("usuarios", usuarioDao.buscaPaginada(pagina));
 		result.include("count", usuarioDao.count());
+	}
+	
+	@Post("/produtos/filtrar")
+	public void filtra(@NotBlank(message = "{login.obrigatorio}") String login) {
+		validator.onErrorRedirectTo(this).usuarios();
+		
+		result.include("usuarios", usuarioDao.buscaPorLoginLike(login));
+		result.of(this).usuarios();
 	}
 	
 }
